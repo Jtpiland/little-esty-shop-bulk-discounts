062bc16580a4ae152145751710553d2d3d247b36
Jacob Piland <piland.jacob@gmail.com>
2021-08-07 13:15:31 -0500

    add show spec for merchants bulk discount

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Bulk Discount Shwo Page' do
+  before :each do
+    @merchant_1 = Merchant.create!(name: 'Sparkys Shop')
+
+    @discount_1 = @merchant_1.bulk_discounts.create!(percentage: 20, quantity: 15)
+    @discount_2 = @merchant_1.bulk_discounts.create!(percentage: 15, quantity: 10)
+
+    visit merchant_bulk_discount_path(@merchant_1, @discount_1)
+  end
+
+  it 'can display the attributes of a particular discount' do
+    expect(page).to have_content(@discount_1.id)
+    expect(page).to have_content(@discount_1.percentage)
+    expect(page).to have_content(@discount_1.quantity)
+    expect(page).to_not have_content(@discount_2.id)
+  end
+end
