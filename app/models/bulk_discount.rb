264fdbaa386ee6ff6880d23993449f73f37305de
Jacob Piland <piland.jacob@gmail.com>
2021-08-08 11:56:24 -0500

    add platform x86_84-linux to try and resolve heroku push issue

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,5 @@ class BulkDiscount < ApplicationRecord
   validates :percentage, :numericality => {:only_integer => true}
   validates :quantity, :numericality => {:only_integer => true}
 
+  #validates :percentage, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
 end
